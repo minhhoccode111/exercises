c0cba00378d609f0a32719b5450eaefabd9ef6c2
minhhoccode111 <minhhoccode111@gmail.com>
2023-08-11 20:57:28 +0700

    merge sort version 1 first time tried

--- README.md
+++ README.md
@@ -6,3 +6,17 @@ Solutions to the Odin Project Node path, Javascript course, DSA sub-course, Proj
 
 Open the file and type in `node [path-to-file]` in the terminal.
 The answer should correspond to the commented code result!
+
+## Merge sort
+
+In merge sort, the idea of the algorithm is to sort smaller arrays and then combine those arrays together (merge them) in sorted order.
+Merge sort leverages something called recursion.
+
+## Merge sort Pseudo-code
+
+- a function named mergeSort which a an unsorted array as argument
+- check if that arr's length is less than 2
+  - if yes, then return the array because it's already been sorted
+- if not then we divide the array to 2 halves
+- then recursively pass each half as argument into the mergeSort function again
+- then merge 2 sort halves back together
---
+++ assignment-2/merge-sort.js
@@ -0,0 +1,53 @@
+// Build a function "mergeSort" that takes in an array and returns a sorted array, using a recursive merge sort methodology.
+
+// Tips:
+
+// Think about what the base case is and what behavior is happening again and again and can actually be delegated to someone else (e.g. that same function!).
+
+// It may be helpful to checkout the background videos again if you don't quite understand what should be going on.
+
+const sort = (arr1, arr2, arr = [], i = 0, j = 0, k = 0) => {
+  const item1 = arr1[i];
+  const item2 = arr2[j];
+  const l1 = arr1.length;
+  const l2 = arr2.length;
+  if (i === l1 && j === l2) return arr;
+  if ((i === l1 && j < l2) || item1 >= item2) {
+    arr[k] = item2;
+    return sort(arr1, arr2, arr, i, j + 1, k + 1);
+  }
+  if ((i < l1 && j === l2) || item1 < item2) {
+    arr[k] = item1;
+    return sort(arr1, arr2, arr, i + 1, j, k + 1);
+  }
+};
+
+const mergeSort = (arr) => {
+  // const arr = [1, 2, 3, 4, 5, 6, 7];
+
+  const length = arr.length;
+
+  if (length < 2) return arr;
+
+  const middle = Math.floor(length / 2);
+
+  // right always greater or equal
+  const right = arr.splice(middle, length - 1);
+
+  const arr1 = mergeSort(arr);
+
+  // so arr2 greater or equal
+  const arr2 = mergeSort(right);
+
+  return sort(arr1, arr2);
+};
+
+const list0 = [2, 6, 7, 71, 1, 23, 13, 5, 14, 23, 3, 5, 7, 8, 97, 89];
+const list1 = [2, 4, 6, 7, 8, 1, 9, 1, 64, 79, 6, 3, 2, 2, 3, 7, 3, 2];
+const list2 = [8, 7, 6, 5, 4, 3, 2, 1];
+const list3 = [8, 7, 6, 5, 4, 3, 2];
+
+console.log(mergeSort(list0)); // [1, 2, 3, 5, 5, 6, 7, 7, 8, 13, 14, 23, 23, 71, 89, 97];
+console.log(mergeSort(list1)); // [1, 1, 2, 2, 2, 2, 3, 3, 3, 4, 6, 6, 7, 7, 8, 9, 64, 79];
+console.log(mergeSort(list2)); // [1, 2, 3, 4, 5, 6, 7, 8];
+console.log(mergeSort(list3)); // [2, 3, 4, 5, 6, 7, 8];
