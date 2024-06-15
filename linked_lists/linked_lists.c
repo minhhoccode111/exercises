a38378f7e47d33cc62ba311f451759f546ac13e5
minhhoccode111 <minhhoccode111@gmail.com>
2024-06-15 23:44:04 +0700

    add: linked lists drawing visualize

--- linked_lists/linked_lists.c
+++ linked_lists/linked_lists.c
@@ -6,39 +6,75 @@ typedef struct node {
   struct node *next;
 } node;
 
+typedef struct list {
+  int size;
+  node *head;
+  node *tail;
+} list;
+
+/*
+ *    head[0]        tail[3]
+ *    |              |
+ *    v              V
+ *    1 -> 2 -> 3 -> 4 -> NULL
+ * ^            ^         ^
+ * |            |         |
+ * prepend      [2]       append
+ */
+
 // TODO: implement: create, append(value), prepend(value), size, head, tail,
 // at(index), pop(), contains(value), find(value), printList(), insertAt(value,
 // index), removeAt(index)
 
-int add(node *list, int val) {
-  node *current = malloc(sizeof(node));
-
-  if (current == NULL) {
-    return -1;
-  }
-
-  current->value = val;
-  current->next = list;
-  list = current;
-  return 0;
+void createList(list *l) {
+  l->size = 0;
+  l->head = NULL;
+  l->tail = NULL;
+  return;
 }
 
-void freeList(node *list) {
-  if (list == NULL) {
-    return;
-  }
-
-  freeList(list->next);
-  free(list);
-}
-
-void printList(node *list) {
+void append(list *l, int val) {
   //
   return;
 }
 
+void prepend(list *l, int val) { return; }
+
+void at(list *l, int index) { return; }
+
+void pop(list *l) { return; }
+
+void contains(list *l, int val) { return; }
+
+void find(list *l, int val) { return; }
+
+void insertAt(list *l, int val, int index) { return; }
+
+void removeAt(list *l, int index) { return; }
+
+void toString(list *l) {
+  printf("head --> ");
+
+  for (node *ptr = l->head; ptr != NULL; ptr = ptr->next) {
+    printf("%d", ptr->value);
+  }
+
+  printf("NULL <-- tail\n");
+  return;
+}
+
+void freeList(node *head) {
+  if (head == NULL) {
+    return;
+  }
+  freeList(head->next);
+  free(head);
+}
+
 int main(void) {
-  node *list = NULL;
+  list *l = NULL;
+
+  createList(l);
 
   return 0;
 }
